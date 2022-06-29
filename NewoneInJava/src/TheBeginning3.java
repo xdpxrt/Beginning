4db7edad1d5deed8a848af82fa5afbce36762b01
xd_pxrt <xdpxrt@yandex.ru>
2022-06-29 12:37:04 +0300

    ggg

--- NewoneInJava/src/TheBeginning3.java
+++ NewoneInJava/src/TheBeginning3.java
@@ -1,27 +1,32 @@
 public class TheBeginning3 {
     public static void main(String[] args) {
-        System.out.println("\t\t\t\t\t  Title | Author\t\t   Year   Pages  How long ro read?!");
+        System.out.println("\t\t\t\t\t\t\t\tTitle | Author\t\t\t Year  Pages  How long ro read?!");
         Book book1 = new Book("Great Mountain", "Scott Wilkins", 1974, 354);
         Book book2 = new Book("Jeffery's Life", "Helen Brooks", 2017, 154);
         Book book3 = new Book();
 
-        book3.setTitle("1917: How's They Gone");
+        book3.setTitle("1917: How They Are Gone");
         book3.setAuthor("Richard Bower");
         book3.setYearOfPublication(1953);
         book3.setNumberOfPages((421));
 
-        Utilities.printInfo(book1.getTitle(), book1.getAuthor(), book1.getYearOfPublication(), book1.getNumberOfPages());
+        Utilities.printInfo(book1.getTitle(), book1.getAuthor(), book1.getYearOfPublication(), book1.getNumberOfPages(),book1.);
         Utilities.printInfo(book2.getTitle(), book2.getAuthor(), book2.getYearOfPublication(), book2.getNumberOfPages());
         Utilities.printInfo(book3.getTitle(), book3.getAuthor(), book3.getYearOfPublication(), book3.getNumberOfPages());
     }
 }
 
 class Utilities {
-    public static void printInfo(String title, String author, int yearOfPublication, int numberOfPages) {
-        System.out.printf("Book added: %15s | %-15s %5d %5dp. \n", title, author, yearOfPublication, numberOfPages);
+    public static int daysToRead(int numberOfPages) {
+        int averagePagesPerDay = 73;
+        return numberOfPages / averagePagesPerDay;
     }
 
-    public static void daysToRead ggg
+    public static void printInfo(String title, String author, int yearOfPublication, int numberOfPages, int daysToRead) {
+        System.out.printf("Book added: %25s | %-15s %5d %5dp. \n", title, author, yearOfPublication, numberOfPages, daysToRead);
+    }
+
+
 }
 
 class Book {
@@ -44,7 +49,7 @@ class Book {
         this.title = title;
     }
 
-    public void setAuthor(String Author) {
+    public void setAuthor(String author) {
         this.author = author;
     }
 
